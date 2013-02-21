7b36a61dc11b00f04495f7c10b95e9fbffc9b52a
Andrew Frankel <andrew@leknarf.net>
2013-02-21 10:31:43 -0500

    Fetch max and min values

--- hn.py
+++ hn.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
+import time
 import requests
 
 HN_API_URL = "http://api.ihackernews.com/"
-NUM_POINTS_TO_CONSIDER = 6
 
+# Based on http://stackoverflow.com/questions/567622/is-there-a-pythonic-way-to-try-something-up-to-a-maximum-number-of-times
+def retry(max_attempts):
+    def try_it(func):
+        def wrapped_func(*args, **kwargs):
+            attempts = 0
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as error:
+                    if attempts >= max_attempts:
+                        raise error
+                    print("{}. Retrying... attempt {}".format(error, attempts))
+                    time.sleep(5)
+                    attempts += 1
+        return wrapped_func
+    return try_it
+
+# The HN api frequently returns 500 errors, so we need to retry errors
+@retry(10)
 def fetch_scores(page_type):
     assert page_type in ['new', 'page']
 
@@ -14,14 +33,15 @@ def fetch_scores(page_type):
         raise Exception("Request for {} failed".format(url))
     score_data = request.json()
     scores = [item['points'] for item in score_data['items']]
-    scores = scores[0:NUM_POINTS_TO_CONSIDER]
 
     return scores
 
 if __name__ == '__main__':
-    ss = fetch_scores('new')
+    highest_new_submissions = max(fetch_scores('new'))
+    lowest_front_page_submissions = min(fetch_scores('page'))
 
-    print(ss)
+    print(highest_new_submissions)
+    print(lowest_front_page_submissions)
 
 
 
